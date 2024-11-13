24e736e65cebf470eeda4c1a497d4e8b412a9184
mgates06 <masongates06@gmail.com>
2024-11-13 08:42:27 -0500

    draw_grid function

--- whackamole.py
+++ whackamole.py
@@ -1,6 +1,28 @@
 import pygame
 
-#First commit (test)
+
+screen = pygame.display.set_mode((640, 512))
+
+
+def draw_grid():
+    #Vertical lines
+    for i in range(1, 20):
+        pygame.draw.line(
+            screen,
+            (0, 0, 0),
+            (i * 32, 0),
+            (i * 32, 512),
+        )
+
+    #Horizontal lines
+    for i in range(1, 16):
+        pygame.draw.line(
+            screen,
+            (0, 0, 0),
+            (0, i * 32),
+            (640, i * 32),
+        )
+
 
 def main():
     try:
@@ -8,16 +30,24 @@ def main():
         # You can draw the mole with this snippet:
         # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
-        screen = pygame.display.set_mode((640, 512))
+        screen
         clock = pygame.time.Clock()
         running = True
+
+
         while running:
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+            screen.fill(color = (15, 185, 220))
+            draw_grid()
             pygame.display.flip()
+
+
             clock.tick(60)
+
+
     finally:
         pygame.quit()
 
